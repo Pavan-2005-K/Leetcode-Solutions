48e22b3b0a542844aa4d3752531f781b1f08337e
Pavan K <pavankpavan14@gmail.com>
2026-09-20 07:40:08 +0530

    Time: 0 ms (100%), Space: 14.9 MB (92.74%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,43 +1,95 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    int getLengthOfLinkedList(ListNode* head){
-        int length=0;
-        while(head!=NULL){
-            length++;
-            head=head->next;
-        }
-        return length;
-    }
-
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int L=getLengthOfLinkedList(head);
-
-        if(n==L){
-            ListNode* temp=head->next;
-            delete(head);
-            return temp;
-        }
-
-        int travel_front=L-n;
         ListNode* temp=head;
-        ListNode* prev=NULL;
-
-        while(travel_front--){
-            prev=temp;
+        for(int i=1;i<=n;i++){
             temp=temp->next;
         }
-        prev->next=temp->next;
-        delete(temp);
+        if(temp==NULL){
+            ListNode* result=head->next;
+            delete(head);
+            return result;
+        }
+
+        ListNode* prev=head;
+
+        while(temp!=NULL&&temp->next!=NULL){
+            prev=prev->next;
+            temp=temp->next;
+        }
+        ListNode* delNode=prev->next;
+        prev->next=prev->next->next;
+        delete(delNode);
+
         return head;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int getLengthOfLinkedList(ListNode* head){
+//         int length=0;
+//         while(head!=NULL){
+//             length++;
+//             head=head->next;
+//         }
+//         return length;
+//     }
+
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         int L=getLengthOfLinkedList(head);
+
+//         if(n==L){
+//             ListNode* temp=head->next;
+//             delete(head);
+//             return temp;
+//         }
+
+//         int travel_front=L-n;
+//         ListNode* temp=head;
+//         ListNode* prev=NULL;
+
+//         while(travel_front--){
+//             prev=temp;
+//             temp=temp->next;
+//         }
+//         prev->next=temp->next;
+//         delete(temp);
+//         return head;
+//     }
+// };
